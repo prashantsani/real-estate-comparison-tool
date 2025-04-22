9610312a724e71cb9eda27ce6ac251e23adfe810
Prashant Sani <prashantsani@users.noreply.github.com>
2025-04-23 01:02:13 +0530

    Revert "ci: update nextjs settings to allow ts file imports"
    This reverts commit 321009be04a218cedbf06ca015811f8704e2353b.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  compiler: {
-    allowImportingTsExtensions: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
